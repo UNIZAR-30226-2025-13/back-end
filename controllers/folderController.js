506dd5cb71d0a7f04cbc5f95beb0674c01eb9554
and <873635@unizar.es>
2025-03-19 12:16:16 +0100

    crear una carpeta

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -30,11 +30,29 @@ const getFolder = async (req, res) => {
     }
 };
 
-module.exports = { getFolder };
+// Crear una carpeta y asociarla a un usuario
+const createFolder = async (req, res) => {
+    try {
+        const { nombre_carpeta, nombre_usuario } = req.body; // obtener el nombre de la carpeta a crear y el nombre del usuario al que asociarla
+        
+        if (!nombre_usuario || !nombre_carpeta) { // ningun campo vacio
+            return res.status(400).json({ message: "Hay que rellener todos los campos" });
+        }
 
+        // comprobar si el usuario existe
+        const result = await client.execute("SELECT * FROM Usuario WHERE nombre_usuario = ?", [nombre_usuario]);
+        if (result.rows.length == 0) {
+            return res.status(400).json({ message: "El usuario no existe" });
+        }
 
-/*
-    hay que modificar mostrarPlaylists para que sea mostrar lista de reproducción
-    me pasarán un id de la lista de reproducción y yo tendré que devolver las cosas
-    en función de si es una playlist o una lista de episodios
-*/
+        const result_carpeta = await client.execute("INSERT INTO Carpeta (nombre) VALUES (?) RETURNING id_carpeta", [nombre_carpeta]);
+        const id_carpeta = result_carpeta.rows[0].id_carpeta;
+        await client.execute("INSERT INTO Carpetas_del_usuario (nombre_usuario, id_carpeta) VALUES (?, ?)", [nombre_usuario, id_carpeta]);
+        res.status(201).json({ message: "Carpeta creada correctamente" });
+    } catch (error) {
+        console.error("Error al crear la carpeta:", error);
+        res.status(500).json({ message: "Hubo un error al crear la carpeta" });
+    }
+};
+
+module.exports = { getFolder, createFolder };
